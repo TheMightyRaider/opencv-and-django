b4d27dd21a0f2af8ae4f29603355cca15ee7223a
TheMightyRaider <sivasanjay7@gmail.com>
2020-05-09 01:50:57 +0530

    Migrated to Postgresql

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tczsy(uap4e*v83%g!zn0%37c#5mv=rnubn57sj71rbtt^v*jm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':config('NAME'),
+        'USER':config('USER'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('HOST'),
+        'PORT':''
     }
 }
 
--- capstone/imageprocessing/views.py
+++ capstone/imageprocessing/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
+from imutils.video import VideoStream
+from imutils.video import FPS
+import imutils
+import time
 import cv2
 
 # Create your views here.
 
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+print(detector)
 
 def postframes():
-    camera=cv2.VideoCapture(0)
-    while True:
-        success,image=camera.read()
-        ret,jpeg=cv2.imencode('.jpg',image)
-        print(jpeg.tobytes())
+    
+    camera=VideoStream(src=-1).start()
+    time.sleep(2)
 
-        if jpeg is not None:
+    fps=FPS().start()
+
+    while True:
+        frame = camera.read()
+        frame = imutils.resize(frame, width=500)
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+            minNeighbors=5, minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
+
+        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+        print(boxes)
+
+        if boxes is not None:
             yield '{"success":True}'
         else:
             yield "{'success':False}"
@@ -21,3 +42,6 @@ def postframes():
 def videostream(request):
     return StreamingHttpResponse(postframes())
         
+
+# def generateImageEncoding(request):
+
